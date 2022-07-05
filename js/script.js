9bf8e0d2ad17a46f319f3f000a51934f92c47061
Roman <R.rybkin94@gmail.com>
2022-07-05 13:23:44 +0400

    ignore + js

--- js/script.js
+++ js/script.js
@@ -41,23 +41,23 @@
 
 //console.log(4 + +"5");  Унарный = (+"5")
 
-let incr = 10,
-    decr = 10;  
-console.log(incr++);    //Оператор инкримента (увелечение на 1 , ПОСТФИКСНЫЙ = сначало срабатывает incr )
-console.log(decr++);    //Оператор декремента (уменьшение на 1 , ПОСТФИКСНЫЙ = сначало срабатывает decr)
-console.log(++incr);    //Оператор инкримента (увелечение на 1 , ПРЕФИКСНЫЙ = сначало срабатывает сложение)
-console.log(--decr);    //Оператор декремента (уменьшение на 1 , ПРЕФИКСНЫЙ = сначало срабатывает вычитание )
+//let incr = 10,
+//    decr = 10;  
+//console.log(incr++);    //Оператор инкримента (увелечение на 1 , ПОСТФИКСНЫЙ = сначало срабатывает incr )
+//console.log(decr++);    //Оператор декремента (уменьшение на 1 , ПОСТФИКСНЫЙ = сначало срабатывает decr)
+//console.log(++incr);    //Оператор инкримента (увелечение на 1 , ПРЕФИКСНЫЙ = сначало срабатывает сложение)
+//console.log(--decr);    //Оператор декремента (уменьшение на 1 , ПРЕФИКСНЫЙ = сначало срабатывает вычитание )
 
-console.log(5%2); //ОСТАТОК (1)
+//console.log(5%2); //ОСТАТОК (1)
 
 // Оператор &&(И)
-const isChecked = true; 
-const isClose = true;
-console.log(isChecked && isClose);
+//const isChecked = true; 
+//const isClose = true;
+//console.log(isChecked && isClose);
 
 // Оператор || (ИЛИ)
-const isChecked = true; 
-const isClose = true;
-console.log(isChecked || isClose);
+// const isChecked = true; 
+//const isClose = true;
+//console.log(isChecked || isClose);
 
-console.log(2+2*2 != '6'); // Строгое равенство === или !== по типу , Не строгое == или != 
+//console.log(2+2*2 != '6'); // Строгое равенство === или !== по типу , Не строгое == или != 
