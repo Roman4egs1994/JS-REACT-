92d1f1155174cdbe5ae344e55714591e1dfea4e8
Roman <R.rybkin94@gmail.com>
2022-07-05 14:01:41 +0400

    Homework: 1

--- js/script.js
+++ js/script.js
@@ -1,63 +1,45 @@
+/* Задание на урок:
+
+1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
+'Сколько фильмов вы уже посмотрели?'
+
+2) Создать объект personalMovieDB и в него поместить такие свойства:
+    - count - сюда передается ответ на первый вопрос
+    - movies - в это свойство поместить пустой объект
+    - actors - тоже поместить пустой объект
+    - genres - сюда поместить пустой массив
+    - privat - в это свойство поместить boolean(логическое) значение false
+
+3) Задайте пользователю по два раза вопросы:
+    - 'Один из последних просмотренных фильмов?'
+    - 'На сколько оцените его?'
+Ответы стоит поместить в отдельные переменные
+Записать ответы в объект movies в формате: 
+    movies: {
+        'logan': '8.1'
+    }
+
+Проверить, чтобы все работало без ошибок в консоли */
+
+
 "use strict";
 
-//                                                                                  Пример объекта
+const numberOfFilm = +prompt('Cколько фильмов вы уже посмотрели?', '');
 
-//const obj = {
-  //  name: "John",
-    //age: 25,
-    //isMarried: false
-//};
+const  personalMovieDB = { 
+    count: numberOfFilm,
+    movies: {},
+    actors: {},
+    geners: [],
+    privat: false,
+};
 
-//console.log(obj.name);  1 cпособ записи обьекта                             
-//console.log(obj["name"]);  // 2 способ записи обьекта
+const a = prompt('Один из последних просмотренных фильмов?', '');
+const b = prompt('На сколько оцените его?', '');
+const c = prompt('Один из последних просмотренных фильмов?', '');
+const d = prompt('На сколько оцените его?', '');
 
-//                                                                                  Пример Массива 
-//let arr = ['plum.png', 'orange.jpg' , 6 , 'apple.bmp' , {} , []];
-//console.log (arr[1]);
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
-//                                                                                  Вопрос пользователю
-//const result = confirm("Ты здесь?");
-//console.log(result);
-
-
- //                                                                                 Вопрос с ответом        
-//const answer = prompt("Вам есть 18?" , "18");
-//console.log(typeof(answer));
-
-//                                                                                  Создание пустого массива и добавление в него ответов на вопросы
-//const answers = [];
-// answers[0] = prompt('Как ваше имя?', '');
-// answers[1] = prompt('Как ваше фамилия ?', '');
-// answers[2] = prompt('Сколько вам лет?', '');
-
-// document.write(answers);
-
-//                                                                                 ИНТЕРПОЛЯЦИЯ 
-//const category = 'toys';
-//console.log(`http://someurl.com/${category}`);
-
-
-//                                                                                 Операторы
-
-//console.log(4 + +"5");  Унарный = (+"5")
-
-//let incr = 10,
-//    decr = 10;  
-//console.log(incr++);    //Оператор инкримента (увелечение на 1 , ПОСТФИКСНЫЙ = сначало срабатывает incr )
-//console.log(decr++);    //Оператор декремента (уменьшение на 1 , ПОСТФИКСНЫЙ = сначало срабатывает decr)
-//console.log(++incr);    //Оператор инкримента (увелечение на 1 , ПРЕФИКСНЫЙ = сначало срабатывает сложение)
-//console.log(--decr);    //Оператор декремента (уменьшение на 1 , ПРЕФИКСНЫЙ = сначало срабатывает вычитание )
-
-//console.log(5%2); //ОСТАТОК (1)
-
-// Оператор &&(И)
-//const isChecked = true; 
-//const isClose = true;
-//console.log(isChecked && isClose);
-
-// Оператор || (ИЛИ)
-// const isChecked = true; 
-//const isClose = true;
-//console.log(isChecked || isClose);
-
-//console.log(2+2*2 != '6'); // Строгое равенство === или !== по типу , Не строгое == или != 
+console.log(personalMovieDB);
---
+++ js/scriptLvlOne.js
@@ -0,0 +1,83 @@
+"use strict";
+
+//                                                                                  Пример объекта
+
+//const obj = {
+  //  name: "John",
+    //age: 25,
+    //isMarried: false
+//};
+
+//console.log(obj.name);  1 cпособ записи обьекта                             
+//console.log(obj["name"]);  // 2 способ записи обьекта
+
+//                                                                                  Пример Массива 
+//let arr = ['plum.png', 'orange.jpg' , 6 , 'apple.bmp' , {} , []];
+//console.log (arr[1]);
+
+//                                                                                  Вопрос пользователю
+//const result = confirm("Ты здесь?");
+//console.log(result);
+
+
+ //                                                                                 Вопрос с ответом        
+//const answer = prompt("Вам есть 18?" , "18");
+//console.log(typeof(answer));
+
+//                                                                                  Создание пустого массива и добавление в него ответов на вопросы
+//const answers = [];
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваше фамилия ?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+// document.write(answers);
+
+//                                                                                 ИНТЕРПОЛЯЦИЯ 
+//const category = 'toys';
+//console.log(`http://someurl.com/${category}`);
+
+
+//                                                                                 Операторы
+
+//console.log(4 + +"5");  Унарный = (+"5")
+
+//let incr = 10,
+//    decr = 10;  
+//console.log(incr++);    //Оператор инкримента (увелечение на 1 , ПОСТФИКСНЫЙ = сначало срабатывает incr )
+//console.log(decr++);    //Оператор декремента (уменьшение на 1 , ПОСТФИКСНЫЙ = сначало срабатывает decr)
+//console.log(++incr);    //Оператор инкримента (увелечение на 1 , ПРЕФИКСНЫЙ = сначало срабатывает сложение)
+//console.log(--decr);    //Оператор декремента (уменьшение на 1 , ПРЕФИКСНЫЙ = сначало срабатывает вычитание )
+
+//console.log(5%2); //ОСТАТОК (1)
+
+// Оператор &&(И)
+//const isChecked = true; 
+//const isClose = true;
+//console.log(isChecked && isClose);
+
+// Оператор || (ИЛИ)
+// const isChecked = true; 
+//const isClose = true;
+//console.log(isChecked || isClose);
+
+//console.log(2+2*2 != '6'); // Строгое равенство === или !== по типу , Не строгое == или != 
+
+// const numberOfFilm = +prompt('Cколько фильмов вы уже посмотрели?', '');
+
+//const  personalMovieDB = { 
+ //   count: numberOfFilm,
+ //   movies: {},
+  //  actors: {},
+  //  geners: [],
+  //  privat: false,
+//};
+
+//const a = prompt('Один из последних просмотренных фильмов?', '');
+//const b = prompt('На сколько оцените его?', '');
+//const c = prompt('Один из последних просмотренных фильмов?', '');
+//const d = prompt('На сколько оцените его?', '');
+
+//personalMovieDB.movies[a] = b;
+//personalMovieDB.movies[c] = d;
+
+//console.log(personalMovieDB);
