d010b8c3bec0e5764c0fa1b276cb76182f2af936
Roman <R.rybkin94@gmail.com>
2022-08-18 08:28:27 +0400

    052 JSON

---
+++ js/052_FormatJs.js
@@ -0,0 +1,22 @@
+"use strict";
+
+//JSON - формат передачи текстовых данных
+
+const persone = {
+    name: 'Alex',
+    tel: '+744444444',
+    parents: {
+        mom: 'Olga',
+        dad: 'Mike'
+    }
+};
+
+// console.log(JSON.stringify(persone)); //Метод отправки JSON
+// console.log(JSON.parse(JSON.stringify(persone))); //Получаем обьект обратно
+
+
+//Создание ГЛУБОКОГО КЛОНА
+const clone = JSON.parse(JSON.stringify(persone)); //JSON.stringify(persone))  = превращает существующий обьект в формат JSON //JSON.parse = распарсит обратно в обьект 
+clone.parents.mom = 'Ann';
+console.log(persone);
+console.log(clone);
